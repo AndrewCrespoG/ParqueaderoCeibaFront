5e4cdb79a4a2592b3eefa91b18fd6675bd19c21a
Andres Crespo <andrescrespo5030@gmail.com>
2018-05-31 17:07:39 -0500

    Agregagr librerias de bootstrap y ngx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TabsModule } from 'ngx-bootstrap/tabs';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TabsModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
